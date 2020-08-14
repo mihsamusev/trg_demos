c35201090c12fb4d11e32fa7e33570dc26d61b5b
Pelle Rosenbeck Gøeg <5758748+pellegoeg@users.noreply.github.com>
2020-08-14 10:03:08 +0200

    Changed segment_gnss into insert rather than update

--- gps_gnss/grouping/segment_gnss_into_groups.sql
+++ gps_gnss/grouping/segment_gnss_into_groups.sql
@@ -1,35 +1,39 @@
 --- Segmenting GNSS timeseries data into trips by time
 --- Tablename tablename is holding the datapoints
 --- 
+
+--- Updating a large table of data is timeconsuming, so the original table is copied to tablename_old
+--- Then a new table is created with tablename_old but with a new column group_id
+
 --- How To:
---- Update tablename with name of table holding the datapoints.
---- 
+--- 1. Update tablename with name of table holding the datapoint in ~ line 12, 13, 15, 31, 78 and 79
 
-/*
--- Add group_id attribute to geopoints table
+--- 2. Move original table to suffix _old:
+--- ALTER TABLE tablename RENAME tablename TO tablename_old;
 
-ALTER TABLE tablename
-RENAME tablename TO tablename_old;
+--- 3. Create new table like original, but with group_id column
+--- CREATE TABLE IF NOT EXISTS tablename ( LIKE tablename_old including all, group_id int)
 
-CREATE TABLE tablename ( LIKE tablename_old including all, group_id int)
+--- 4. Run the code from line 22
 
-*/
+--- 5. If succeded, then delete tablename_old
+--- DROP TABLE IF EXIST tablename_old;
 
 with basis as (
-SELECT 
-	indx AS idx,  -- Point index
-	EXTRACT('epoch' from datetime) AS time_in_sec,  -- datetime is timefiled
+select
+	indx as idx,  -- Point index
+	extract('epoch' from datetime) AS time_in_sec,  -- datetime is timefiled
 	datetime AS datetime,
 	device_id AS device_id,
 	geom AS geom,
 	0 as new_group -- Temporary new group identifier
 FROM
 	tablename -- Table keeping GNSS points
-ORDER BY
+order by
 	device_id,
 	indx
 ), diffs as (
-SELECT
+select
 	idx,
 	time_in_sec-lag(time_in_sec) over(order by device_id,
 	idx) as time_diff,
@@ -44,31 +48,33 @@ SELECT
 FROM
 	basis
 ), when_new_groups as (
-SELECT
+select
 	idx,
 	time_diff,
 	spatial_dist,
 	datetime,
 	device_id,
 	/* When to start a new group? */
-    CASE
-      WHEN
+    case
+      when
 		lag_device_id <> device_id  -- different device
-        OR time_diff > 5*60 -- minutes to seconds
-        OR time_diff IS NULL
+        or time_diff > 5*60 -- minutes to seconds
+        or time_diff is null
         --OR spatial_dist > 300 -- in meters
-        THEN new_group + 1
-      ELSE 0
-    END AS new_group
-FROM diffs ),
-groups as (
-SELECT
+        then new_group + 1
+      else 0
+    end as new_group
+from diffs ),
+g as (
+select
 	idx,
 	time_diff,
 	spatial_dist,
 	datetime,
 	device_id,
-	sum(new_group) OVER (PARTITION by device_id ORDER BY device_id asc, datetime ASC) AS group_id
-FROM
+	sum(new_group) over (partition by device_id order by device_id asc, datetime asc) as group_id
+from
 	when_new_groups)
-UPDATE tablename SET group_id = groups.group_id FROM groups WHERE tablename.indx = groups.idx
+insert into tablename
+select org.*, g.group_id::int FROM tablename_old as org JOIN g ON org.indx = g.idx
+
